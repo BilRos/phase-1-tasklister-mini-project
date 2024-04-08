73e9643be9fbcb0b217f9089fc58ebab90d456bc
BilRos <billyrossowuor@gmail.com>
2024-04-08 11:34:14 +0300

    Taskliner

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createNewList(e.target)
+    createNewList(document.querySelector('#new-task-description').value)
+    form.reset()
+  })
 });
+
+
+function createNewList(addOnList){
+function createNewList(addOn){
+  let li = document.createElement('li')
+  li.innerHTML= addOnList;
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteList)
+  btn.textContent = 'Remove ';
+  li.innerHTML= `${addOn} `
+  li.append(btn);
+
+  document.querySelector('#tasks').appendChild(li)
+
+}
+ function deleteList (e){
+  e.target.parentNode.remove();
+ 
+ }}
